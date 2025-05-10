e340e51f3933f17f353bf1375a385b4f237a8f3b
root <muchellealvin@gmail.com>
2025-05-10 22:11:37 +0300

    Update PSQL connection

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,7 @@ app.use(cors());
 app.use(express.json());
 
 // PostgreSQL connection setup
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 
